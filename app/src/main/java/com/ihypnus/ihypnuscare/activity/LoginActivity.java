f916b25ff03cdfa873c99dc25ef1ad2dc0fcf51d
vicky <taovicky@163.com>
2018-05-23 00:38:59 +0800

    1.完成登录界面ui

--- app/src/main/java/com/ihypnus/ihypnuscare/activity/LoginActivity.java
+++ app/src/main/java/com/ihypnus/ihypnuscare/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.text.Editable;
@@ -20,6 +21,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ihypnus.ihypnuscare.R;
@@ -42,20 +44,20 @@ import kr.co.namee.permissiongen.PermissionGen;
 public class LoginActivity extends BaseActivity implements View.OnClickListener {
     private Context context;           //上下文
     private Button mLoginBtn;          //登录按钮
-    private Button mYCodeBtn;            //获取验证码按钮
+    //    private Button mYCodeBtn;            //获取验证码按钮
     private EditText mPhoneEdit;       //手机号输入框
     private EditText mCodeEdit;         //验证码输入框
     private ImageView img_clear;                    //帐号清除
-    private ImageView img_tips;                     //帐号提示
+    //    private ImageView img_tips;                     //帐号提示
     private LinearLayout layout_account;           //帐号内容整体布局
     //自动登录
-    private RelativeLayout layout_choice_autoLogin;
-    private CheckBox cb_autoLogin;
+//    private RelativeLayout layout_choice_autoLogin;
+//    private CheckBox cb_autoLogin;
     //动态码
     private RelativeLayout layout_imgcode;           //动态码布局
     private ImageView mImgCodeIcon;                 //图形动态码
     private Bitmap mCodeBitmap;                     //动态码图片
-    private ImageView mCodeLoading;           // 验证码请求动画imgview
+    //    private ImageView mCodeLoading;           // 验证码请求动画imgview
     private CountDownTimer mCountDownTimerYCode;   // 右边倒计时
     private String isUpdateMacValues;              //登录类型，1永久登录，2一次性登录
 
@@ -68,6 +70,8 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     private String mCode;                           //验证码
     private String mPhoneNumber;                   //手机号
     private final static int REQUEST_CODE = 300;
+    private TextView mTvRegist;
+    private TextView mTvFogotPassWord;
 
     @Override
     protected int setView() {
@@ -79,17 +83,24 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
         mLoginBtn = (Button) this.findViewById(R.id.id_login);
 
         //验证码登录布局控件
-        mYCodeBtn = (Button) this.findViewById(R.id.btn_login_get_code2);
+//        mYCodeBtn = (Button) this.findViewById(R.id.btn_login_get_code2);
         mPhoneEdit = (EditText) this.findViewById(R.id.id_phone_edit2);
         mCodeEdit = (EditText) this.findViewById(R.id.edit_login_code2);
         img_clear = (ImageView) this.findViewById(R.id.img_login_account_clear2);
-        img_tips = (ImageView) this.findViewById(R.id.img_login_account_tips2);
+//        img_tips = (ImageView) this.findViewById(R.id.img_login_account_tips2);
         layout_account = (LinearLayout) this.findViewById(R.id.liner_account_edit2);
         //自动登录
-        layout_choice_autoLogin = (RelativeLayout) findViewById(R.id.layout_auto_login);
-        cb_autoLogin = (CheckBox) findViewById(R.id.cb_auto_login);
+//        layout_choice_autoLogin = (RelativeLayout) findViewById(R.id.layout_auto_login);
+//        cb_autoLogin = (CheckBox) findViewById(R.id.cb_auto_login);
 
-        mCodeLoading = (ImageView) findViewById(R.id.img_code_loading);
+        //注册
+        mTvRegist = (TextView) findViewById(R.id.tv_regist);
+        mTvRegist.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG); //下划线
+        mTvRegist.getPaint().setAntiAlias(true);//抗锯齿
+        //忘记密码
+        mTvFogotPassWord = (TextView) findViewById(R.id.tv_fogot_pass_word);
+        mTvFogotPassWord.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG); //下划线
+        mTvFogotPassWord.getPaint().setAntiAlias(true);//抗锯齿
     }
 
     @Override
@@ -101,9 +112,9 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     protected void initEvent() {
 
         mLoginBtn.setOnClickListener(this);
-        mYCodeBtn.setOnClickListener(this);
+//        mYCodeBtn.setOnClickListener(this);
         img_clear.setOnClickListener(this);
-        img_tips.setOnClickListener(this);
+//        img_tips.setOnClickListener(this);
 
         mPhoneEdit.addTextChangedListener(new TextWatcher() {
             @Override
@@ -120,7 +131,7 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
             public void afterTextChanged(Editable s) {
                 if (s.length() < 1) {
                     mCodeEdit.setText("");
-                    cb_autoLogin.setChecked(false);
+//                    cb_autoLogin.setChecked(false);
                     img_clear.setVisibility(View.GONE);
                     AndroidSystemHelper.ShowKeyboard(mPhoneEdit);
                     if (mCountDownTimerYCode != null) {
@@ -151,22 +162,9 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
                 }
             }
         });
-        layout_choice_autoLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (cb_autoLogin.isChecked()) {
-                    cb_autoLogin.setChecked(false);
-                } else {
-                    cb_autoLogin.setChecked(true);
-                }
-            }
-        });
-        cb_autoLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                isAutoLogin = b;
-            }
-        });
+
+        mTvRegist.setOnClickListener(this);
+        mTvFogotPassWord.setOnClickListener(this);
     }
 
     @Override
@@ -215,65 +213,31 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
                 mPhoneEdit.setText("");
                 mCodeEdit.setText("");
                 if (isAutoLogin) {
-                    cb_autoLogin.setChecked(false);
+//                    cb_autoLogin.setChecked(false);
                 }
                 break;
-            // 验证码登录时获取历史帐号
-            case R.id.img_login_account_tips2:
 
-
-                break;
-            //  验证码登录获取验证码
-            case R.id.btn_login_get_code2:
-                if (!ViewUtils.isFastDoubleClick()) {
-                    String error = checkRequire2();
-                    if (error.equals("")) {
-                        mCodeLoading.setVisibility(View.VISIBLE);
-                        mCodeLoading.startAnimation(mCodeLoadingAnim);
-                        mYCodeBtn.setText("正在获取");
-                        AndroidSystemHelper.HideKeyboard(LoginActivity.this);
-                        getUserConfirm2();
-                    } else {
-                        Toast.makeText(LoginActivity.this, error, Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                }
-                break;
             //登录
             case R.id.id_login:
                 if (!ViewUtils.isFastDoubleClick()) {
                     Intent intent = new Intent(LoginActivity.this, CaptureActivity.class);
                     startActivityForResult(intent, 121);
-//                    IhyRequest.getUserConfirm2(new Response.Listener<Object>() {
-//                        @Override
-//                        public void onResponse(Object response) {
-//                            CarSanitationListBean bean = (CarSanitationListBean) response;
-//                            if (bean != null && bean.getResult() != null && bean.getResult().getDatas() != null) {
-//                                ToastUtils.showToastDefault(LoginActivity.this, bean.getErrMsg());
-//                                LogOut.d("llw", bean.getResult().getDatas().toString());
-//                            }
-//                        }
-//                    }, new Response.ErrorListener() {
-//                        @Override
-//                        public void onErrorResponse(VolleyError error) {
-//                            ToastUtils.showToastDefault(LoginActivity.this, error.toString());
-//                        }
-//                    });
-                  /*  UserInfo userInfo = new UserInfo();
-                    userInfo.setPhone("18820000743");
-                    userInfo.setPassword("888888");
-                    String userInfoJson = new Gson().toJson(userInfo);
-                    IhyRequest.getUserRegister(userInfoJson, "CP70100511S", new Response.Listener<Object>() {
-                        @Override
-                        public void onResponse(Object response) {
-//                            ToastUtils.showToastDefault(LoginActivity.this, response);
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            ToastUtils.showToastDefault(LoginActivity.this, error.toString());
-                        }
-                    });*/
+                }
+                break;
+            //注册
+            case R.id.tv_regist:
+                if (!ViewUtils.isFastDoubleClick()) {
+                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivityForResult(intent, 122);
+                }
+                break;
+
+            //忘记密码
+            case R.id.tv_fogot_pass_word:
+                if (!ViewUtils.isFastDoubleClick()) {
+                    ToastUtils.showToastDefault(LoginActivity.this, "找回密码");
+//                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+//                    startActivityForResult(intent, 122);
                 }
                 break;
 
@@ -294,13 +258,13 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
      * 清除验证码加载动画
      */
     private void clearCodeLoaidng() {
-        mCodeLoading.setVisibility(View.GONE);
-        mCodeLoading.clearAnimation();
-        mYCodeBtn.setEnabled(true);
+//        mCodeLoading.setVisibility(View.GONE);
+//        mCodeLoading.clearAnimation();
+//        mYCodeBtn.setEnabled(true);
     }
 
     public void getUserConfirm2() {
-        mYCodeBtn.setEnabled(false);
+//        mYCodeBtn.setEnabled(false);
         // TODO: 2018/5/15  调登入接口
     }
 
